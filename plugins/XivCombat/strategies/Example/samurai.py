7a1b5d901e758af9c7e6dfa9eaeaf6267cc703cd
AutumnInSouth <wuyangtuo@gmail.com>
2021-11-16 03:39:27 -0800

    Fix bug: OpenEyes; add Higanbana; cache value of gcd_total

--- plugins/XivCombat/strategies/Example/samurai.py
+++ plugins/XivCombat/strategies/Example/samurai.py
@@ -8,7 +8,8 @@ samurai_auras = {
     'Jinpu': 1298,
     'Shifu': 1299,
     'Higanbana': 1228,
-    'Kaiten': 1229
+    'Kaiten': 1229,
+    'OpenEyes': 1252
 }
 
 
@@ -17,19 +18,22 @@ class SamuraiLogic(Strategy):
     fight_only = False
     job = 'Samurai'
     default_data = {}
+    gcd = 0
 
     def global_cool_down_ability(self, data: 'LogicData'):
 
         # to make a decision, we always need quite a lot information first
         lv = data.me.level
-        gcd = data.gcd
+        self.gcd = data.gcd_total if data.gcd_total > 0 else self.gcd
         gauge = data.gauge
         effects = data.effects
+        target_effects = data.target.effects.get_dict(source=data.me.id)
         num_sen = sum([gauge.moon, gauge.flower, gauge.snow])
         combo_id = data.combo_id
         kenki = gauge.kenki
         jinpu_remain = effects[samurai_auras['Jinpu']].timer if samurai_auras['Jinpu'] in effects else 0
         shifu_remain = effects[samurai_auras['Shifu']].timer if samurai_auras['Shifu'] in effects else 0
+        higanbana_remain = target_effects[samurai_auras['Higanbana']].timer if samurai_auras['Higanbana'] in target_effects else 0
 
         # huh, less codes, better codes
         def use_ability_to_target(spell_name: str):
@@ -53,11 +57,11 @@ class SamuraiLogic(Strategy):
             elif not gauge.snow:
                 return use_ability_to_target('Yukikaze')
             else:
-                if samurai_auras['Kaiten'] in effects and samurai_auras['Jinpu'] in effects:
+                if samurai_auras['Kaiten'] in effects and jinpu_remain > 0:
                     return use_ability_to_target('MidareSetsugekka')
-                if samurai_auras['Kaiten'] not in effects and samurai_auras['Jinpu'] in effects:
+                if samurai_auras['Kaiten'] not in effects and jinpu_remain > 0:
                     if kenki >= 20:
-                        return [use_ability_to_target('HissatsuKaiten'), use_ability_to_target('MidareSetsugekka')]
+                        return use_ability_to_target('HissatsuKaiten')
                     else:
                         # TODO: to collect kenki, we'd better consider player's facing
                         return use_ability_to_target('Hakaze')
@@ -65,13 +69,13 @@ class SamuraiLogic(Strategy):
         # second, if now we got 3 sens, consider using midare setsugekka
         if num_sen == 3:
             # if we got Jinpu aura
-            if samurai_auras['Jinpu'] in effects:
+            if jinpu_remain > 0:
                 # TODO: in some special case, we may wait for kaeshi setsugekka
                 # if kenki is enough for Kaiten, Midare Setsugekka!
                 if samurai_auras['Kaiten'] in effects:
                     return use_ability_to_target('MidareSetsugekka')
                 if kenki >= 20:
-                    return [use_ability_to_target('HissatsuKaiten'), use_ability_to_target('MidareSetsugekka')]
+                    return use_ability_to_target('HissatsuKaiten')
                 # sadly, we do not have enough kenki, go get some!
                 elif combo_id == samurai_spells['Shifu']['id']:
                     return use_ability_to_target('Kasha')
@@ -85,9 +89,22 @@ class SamuraiLogic(Strategy):
             else:
                 return use_ability_to_target('Jinpu' if combo_id == samurai_spells['Hakaze']['id'] else 'Hakaze')
 
+        if num_sen == 1 and jinpu_remain > 0 and higanbana_remain < self.gcd * 2:
+            if samurai_auras['Kaiten'] in effects:
+                return use_ability_to_target('Higanbana')
+            if kenki >= 20:
+                return use_ability_to_target('HissatsuKaiten')
+            elif combo_id == samurai_spells['Shifu']['id']:
+                return use_ability_to_target('Kasha')
+            elif combo_id == samurai_spells['Jinpu']['id']:
+                return use_ability_to_target('Gekko')
+            elif combo_id == samurai_spells['Hakaze']['id']:
+                return use_ability_to_target('Jinpu' if jinpu_remain <= shifu_remain else 'Shifu')
+            else:
+                return use_ability_to_target('Hakaze')
+        
         # third, in general, if we are in middle of a combo, we finish it
         if combo_id == samurai_spells['Shifu']['id']:
-            # TODO: consider higanbana
             return use_ability_to_target('Kasha')
 
         if combo_id == samurai_spells['Jinpu']['id']:
@@ -129,7 +146,8 @@ class SamuraiLogic(Strategy):
         gauge = data.gauge
         effects = data.effects
         kenki = data.gauge.kenki
-        gcd = data.gcd
+        self.gcd = data.gcd_total if data.gcd_total > 0 else self.gcd
+        jinpu_remain = effects[samurai_auras['Jinpu']].timer if samurai_auras['Jinpu'] in effects else 0
 
         if kenki < 50 and skill_cd('Ikishoten') == 0:
             return use_ability_to_target('Ikishoten')
@@ -137,15 +155,15 @@ class SamuraiLogic(Strategy):
         if gauge.meditation > 2:
             return use_ability_to_target('Shoha')
 
-        if samurai_auras['Jinpu'] in effects and kenki >= 70 and skill_cd('HissatsuSenei') == 0:
+        if jinpu_remain > 0 and kenki >= 70 and skill_cd('HissatsuSenei') == 0:
             return use_ability_to_target('HissatsuSenei')
 
-        if (skill_cd('HissatsuSenei') > skill_cd('Ikishoten') or skill_cd('HissatsuSenei') > 6 * gcd or kenki >= 85) and \
+        if (skill_cd('HissatsuSenei') > skill_cd('Ikishoten') or skill_cd('HissatsuSenei') > 6 * self.gcd or kenki >= 85) and \
             kenki >= 35 and \
-            samurai_auras['Kaiten'] in effects:
+            samurai_auras['OpenEyes'] in effects:
             return use_ability_to_target('HissatsuSeigan')
 
-        if (skill_cd('HissatsuSenei') > skill_cd('Ikishoten') or skill_cd('HissatsuSenei') > 6 * gcd or kenki >= 95) and kenki >= 45:
+        if (skill_cd('HissatsuSenei') > skill_cd('Ikishoten') or skill_cd('HissatsuSenei') > 6 * self.gcd or kenki >= 95) and kenki >= 45:
             return use_ability_to_target('HissatsuShinten')
         # To make a decision, we need quit a lot information
 
