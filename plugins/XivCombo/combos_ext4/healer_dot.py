e33b2d13f706476add4d6a38b95147bb92c28bb2
ny <1>
2022-01-07 16:56:17 +0800

    1

--- plugins/XivCombo/combos_ext4/healer_dot.py
+++ plugins/XivCombo/combos_ext4/healer_dot.py
@@ -39,9 +39,9 @@ def ast(me):
 sge_dot = {2614, 2615, 2616}
 
 
-@combo_func(24290, "sge/dot", "Eukrasian Dosis III 替换 gcd")
+@combo_func(24312, "sge/dot", "gcd 替换 dot")
 def sge(me):
-    return 24283 if me.level < 30 or target_has_dot(me.id, sge_dot) < 2.5 and not me.effects.has(2606) else 24290
+    return 24283 if me.level < 30 or target_has_dot(me.id, sge_dot) > 2.5 or me.effects.has(2606) else 24290
 
 
 combos = [whm, sch, ast, sge]
