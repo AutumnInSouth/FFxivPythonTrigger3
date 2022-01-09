f4717a8620eb6c38ba085dbfa8e3c1813004bbe4
ny <1>
2022-01-09 22:55:56 +0800

    1

--- plugins/RaidHelper/ext4/p3s.py
+++ plugins/RaidHelper/ext4/p3s.py
@@ -8,13 +8,14 @@ if TYPE_CHECKING:
 
 p3s = map_trigger(1007)
 icon_lock = Lock()
+target_icon_evt_id = 'network/zone/server/actor_control/target_icon'
 
 last_rec = 0
 rec_icons = []
 
 
-@p3s('点名指挥', 'network/zone/server/actor_control/target_icon')
-def p3s_target_icon_1(output, e: 'TargetIconEvent') -> None:
+@p3s('点名指挥', target_icon_evt_id)
+def p3s_target_icon(output, e: 'TargetIconEvent') -> None:
     global last_rec
     if 268 <= e.icon_id <= 276:
         with icon_lock:
@@ -24,16 +25,24 @@ def p3s_target_icon_1(output, e: 'TargetIconEvent') -> None:
             rec_icons.append((e.target_actor.job.short_name, e.icon_id - 268))
             if len(rec_icons) >= 8:
                 rec_icons.sort(key=lambda x: x[1])
-                output(f"""
-(4){rec_icons[3][0]} | {rec_icons[7][0]}    {rec_icons[0][0]} | {rec_icons[4][0]}(1)
-                    BOSS
-(3){rec_icons[2][0]} | {rec_icons[6][0]}    {rec_icons[1][0]} | {rec_icons[5][0]}(2)
-""")
+                output(f"(1) {rec_icons[0][0]} {rec_icons[4][0]}", in_game_output=2)
+                output(f"(2) {rec_icons[1][0]} {rec_icons[5][0]}", in_game_output=2)
+                output(f"(3) {rec_icons[2][0]} {rec_icons[6][0]}", in_game_output=2)
+                output(f"(4) {rec_icons[3][0]} {rec_icons[7][0]}", in_game_output=2)
 
 
-@p3s('点名瞬移', 'network/zone/server/actor_control/target_icon')
-def p3s_target_icon_2(output, e: 'TargetIconEvent') -> None:
-    global last_rec
-    if 268 <= e.icon_id <= 276 and e.target_id == plugins.XivMemory.player_info.id:
-        p = getattr(plugins.XivMemory.markings.way_mark, ['one', 'two', 'three', 'four'][(e.icon_id - 268) % 4])
-        if p.is_active: plugins.XivMemory.coordinate.set(p.x, p.y, p.z)
+def dark_fire(points=None):
+    def func(output, e: 'TargetIconEvent') -> None:
+        if 268 <= e.icon_id <= 276 and e.target_id == plugins.XivMemory.player_info.id:
+            i = (e.icon_id - 268) % 4
+            if points:
+                p = getattr(plugins.XivMemory.markings.way_mark, points[i])
+                if p.is_active: plugins.XivMemory.coordinate.set(p.x, p.y, p.z)
+            output(f"point {i + 1}")
+
+    return func
+
+
+p3s_target_icon_tp_num = p3s('点名瞬移(数字标点)', target_icon_evt_id)(dark_fire(['one', 'two', 'three', 'four']))
+p3s_target_icon_tp_abc = p3s('点名瞬移(字母标点)', target_icon_evt_id)(dark_fire(['a', 'b', 'c', 'd']))
+p3s_target_icon_tell = p3s('点名提示', target_icon_evt_id)(dark_fire())
