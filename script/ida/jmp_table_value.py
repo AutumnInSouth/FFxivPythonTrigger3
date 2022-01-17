877687128d0b1724f23a77bb42e3aca59733b81e
ny <1>
2022-01-17 12:03:22 +0800

    1

--- script/ida/jmp_table_value.py
+++ script/ida/jmp_table_value.py
@@ -2,8 +2,7 @@ from idc import *
 from idaapi import *
 from idautils import *
 
-test_sig = "48 8B CB E8 ? ? ? ? B0 ? 48 8B 5C 24 ? 48 8B 74 24 ? 48 83 C4 ? " \
-           "5F C3 48 8B CB E8 ? ? ? ? B0 ? 48 8B 5C 24 ? 48 8B 74 24 ? 48 83 C4 ? 5F C3 0F B6 43 ?"
+test_sig = "33 D2 48 8B CE E8 ? ? ? ? B0 ? 48 8B 5C 24 ?"
 min_ea = inf_get_min_ea()
 max_ea = inf_get_max_ea()
 
@@ -32,24 +31,26 @@ def _find_switch_values(_si: int, ea: int):
             if results.targets[idx] == ea:
                 cases = results.cases[idx]
                 return [cases[idx] for idx in range(len(cases))]
-    return None
+    return []
 
 
 def find_switch_values(ea: int):
     xrefs = find_xrefs(ea)
     while True:
         if 19 in xrefs:
-            res = _find_switch_values(xrefs[19][0], ea)
-            if res is not None:
-                return res
+            res = sum((_find_switch_values(si, ea) for si in xrefs[19]),[])
+            if res: return sorted(res)
         if 21 in xrefs:
             ea = xrefs[21][0]
             xrefs = find_xrefs(ea)
+        elif 19 in xrefs:
+            ea = xrefs[19][0]
+            xrefs = find_xrefs(ea)
         else:
-            raise Exception('not found')
+            raise Exception(f'not found in {list(xrefs.keys())}')
 
 
 for addr in sig_search(test_sig):
     values = find_switch_values(addr)
-    print(hex(addr), values)
+    print(hex(addr), "|".join(map(hex, values)))
 print('done')
